01d7fdc29772ca218804f6886d43bea9a94b83e2
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-04-28 22:19:07 -0700

    added some styling

--- src/App.text.jsx
+++ src/App.text.jsx
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 import './App.css';
 describe('App', () => {
-  it('should display a list of quotes', async () => {
-    render(<App />);
+  it('should display a list of quotes adn filter by character', async () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
   });
 });
